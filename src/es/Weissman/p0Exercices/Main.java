185dcbcc26405cfe4012aee1b10b3b7219f8304e
Javier Blanco <javitxuweiss@gmail.com>
2020-05-05 17:00:06 +0200

    Sixth Exercise

--- src/es/Weissman/p0Exercices/Main.java
+++ src/es/Weissman/p0Exercices/Main.java
@@ -57,6 +57,28 @@ public class Main {
         System.out.println(String.format("Input second number: %d", B));
         System.out.println("Expected output:");
         System.out.println(String.format("%d x %d = %d",A,B,A*B));*/
+        /*
+        6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers. Go to the editor
+        Test Data:
+        Input first number: 125
+        Input second number: 24
+        Expected Output :
+        125 + 24 = 149
+        125 - 24 = 101
+        125 x 24 = 3000
+        125 / 24 = 5
+        125 mod 24 = 5
+         */
+        /*int A=125, B=24;
+        System.out.println("Test Data:");
+        System.out.println(String.format("Input first number: %d", A));
+        System.out.println(String.format("Input second number: %d", B));
+        System.out.println("Expected output:");
+        System.out.println(String.format("%d + %d = %d",A,B,A+B));
+        System.out.println(String.format("%d - %d = %d",A,B,A-B));
+        System.out.println(String.format("%d x %d = %d",A,B,A*B));
+        System.out.println(String.format("%d / %d = %d",A,B,A/B));
+        System.out.println(String.format("%d mod %d = %d",A,B,A%B));*/
         
     }
 }
