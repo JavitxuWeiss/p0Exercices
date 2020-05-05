fad89d837467585630d20a82fa31f995b8b67adc
Javier Blanco <javitxuweiss@gmail.com>
2020-05-05 16:10:19 +0200

    Second Exercise

--- src/es/Weissman/p0Exercices/Main.java
+++ src/es/Weissman/p0Exercices/Main.java
@@ -3,8 +3,14 @@ package es.Weissman.p0Exercices;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        System.out.println("Hola Petardilla\nAdios petardilla!!");
+	// exercise 1 - write hello and name in second line
+        // System.out.println("Hola Petardilla\nAdios petardilla!!");
+    // exercise 2 - write A + B numbers in second line "expected outout" and in third line the result
+        int A,B;
+        A= 74;
+        B= 36;
+        System.out.println(A + " + " +B);
+        System.out.println("Expected output:");
+        System.out.println(A+B);
     }
 }
