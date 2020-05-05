e61394653359236170c722a8f877f4fe8b4e019e
Javier Blanco <javitxuweiss@gmail.com>
2020-05-05 16:01:13 +0200

    firs commit java exercices basic1

---
+++ src/es/Weissman/p0Exercices/Main.java
@@ -0,0 +1,10 @@
+package es.Weissman.p0Exercices;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hola Petardilla\nAdios petardilla!!");
+    }
+}
