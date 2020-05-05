0a46723dd236b1ea41d0a55022f548f9ce1798d3
Javier Blanco <javitxuweiss@gmail.com>
2020-05-05 16:49:54 +0200

    Fourth Exercise

--- src/es/Weissman/p0Exercices/Main.java
+++ src/es/Weissman/p0Exercices/Main.java
@@ -19,7 +19,30 @@ public class Main {
 //        System.out.println(A + "/" +B);
 //        System.out.println("Expected output:");
 //        System.out.println((A/B));
-    //
+    /*4. Write a Java program to print the result of the following operations
+        Test Data:
+        a. -5 + 8 * 6
+        b. (55+9) % 9
+        c. 20 + -3*5 / 8
+        d. 5 + 15 / 3 * 2 - 8 % 3
+        Expected Output :
+        43
+        1
+        19
+        13 */
+        int A=-5,B=8,C=6;
+        System.out.println(A + " + "+ B + " * "+ C);
+        int E=55,D=9;
+        System.out.println("(" + E+ "+"+D+") % "+D);
+        int F=20, G=-3, H=5, I=8;
+        System.out.println(String.format("%d + %d*%d / %d",F,G,H,I));
+        int J=5,K=15,L=3,M=2,N=8,O=3;
+        System.out.println(String.format("%d + %d / %d * %d - %d %% %d",J,K,L,M,N,O));
+        System.out.println("Expected output:");
+        System.out.println(A+B*C);
+        System.out.println((E+D)%D);
+        System.out.println(F+G*H/I);
+        System.out.println(J +K / L * M - N %O);
 
     }
 }
